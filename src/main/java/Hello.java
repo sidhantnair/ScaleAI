4d29d1faf45aa2a6c41922a56b7ab0592cd2e2ae
Sidhant Nair <nair.sidhant@gmail.com>
2022-06-17 02:29:55 -0400

    added sample app with springboot support

--- src/main/java/Hello.java
+++
@@ -1,7 +0,0 @@
-public class Hello {
-
-    public Boolean returnTrue() {
-        return true;
-    }
-
-}
---
+++ src/main/java/com/sidhantnair/ScaleAI/ScaleAIApplication.java
@@ -0,0 +1,13 @@
+package com.sidhantnair.ScaleAI;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ScaleAIApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ScaleAIApplication.class, args);
+    }
+
+}
